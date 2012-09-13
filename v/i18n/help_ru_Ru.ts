a645b6e34e0f32cd5a43358810ee6b099f1d93d5
dae-eklen <dae.eklen@gmail.com>
2012-09-13 13:01:58 +0300

    first commit

---
+++ v/i18n/help_ru_Ru.ts
@@ -0,0 +1,23 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="ru_RU">
+<context>
+    <name>Dialog</name>
+    <message>
+        <source>Help</source>
+        <translation type="obsolete">Помощь</translation>
+    </message>
+    <message>
+        <source>Return to previous page</source>
+        <translation type="obsolete">Вернуться на предыдущую страницу</translation>
+    </message>
+    <message>
+        <source>Go to home page</source>
+        <translation type="obsolete">На главную страницу</translation>
+    </message>
+    <message>
+        <source>Home page</source>
+        <translation type="obsolete">Главная страница</translation>
+    </message>
+</context>
+</TS>
